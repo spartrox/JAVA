eb9ff61679f31d80024982cc78a9deba677f94ca
Allan Boite <allan.boite@gmail.com>
2020-07-23 22:05:54 +0200

    autre sauvegarde

--- projet3/src/projet3/Main.java
+++ projet3/src/projet3/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Player player1 = new Player("Spartrox", 30, 400);
-		System.out.println("Vie de départ : " + player1.getHealth() + " .");
+		System.out.println("Vie de départ : " + player1.getHealth() + " . ");
 		player1.damage(50);
-		System.out.println("Point de vie : " + player1.getHealth() + " .");
+		System.out.println("Point de vie : " + player1.getHealth() + " . ");
 		
 
 
