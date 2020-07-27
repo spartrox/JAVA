e1beb59719a4b1403da1e80f0ef4c7ba574d4ad6
Allan Boite <allan.boite@gmail.com>
2020-07-27 22:36:12 +0200

    énumération JAVA

--- projet3/src/projet3/Day.java
+++ projet3/src/projet3/Day.java
@@ -2,16 +2,26 @@ package projet3;
 
 public enum Day {
 	
-	MATIN(8), MIDI(12), APREMIDI(15), SOIR(22), NUIT(2);
+	MATIN(8, " Il est l'heure de se lever"),
+	MIDI(12, " A table !"),
+	APRESMIDI(15," C'est l'heure du gouter"),
+	SOIR(22, " Bonne nuit"),
+	NUIT(2, " Fais de beau rêve");
 
 	private int heure;
+	private String message;
 	
-	Day(int heure){
+	Day(int heure, String message){
 		this.heure = heure;
+		this.message = message;
 		
 	}
 	
 	public int getHeure() {
 		return heure;
+	}
+	
+	public String getMessage() {
+		return message;
 	}
 }
--- projet3/src/projet3/Main.java
+++ projet3/src/projet3/Main.java
@@ -4,22 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		PineApple a = new PineApple();
-		a.hasSeed();
-		a.miam();
-		a.taste();
-		
-		if(!(a instanceof PeelFruit)) {
-			System.out.println("Ce fruit ne fais rien .");
+		for(Day moment : Day.values()) {
+			System.out.println("Nous sommes actuellement le " + moment.name().toLowerCase() + " il est actuellement : " + moment.getHeure() + "h ." + moment.getMessage());
 		}
 		
-		Day moment = Day.MATIN;
 		
-		System.out.println(moment.name());
 		
-		moment = Day.MIDI;
 		
-		System.out.println("Nous sommes actuellement le " + moment.name() + " il est actuellement : " + moment.getHeure() + " h .");
 		
 		
 		
