ea6abbd180716e1cf1978a4a62d2f1bedc4dd06f
Allan Boite <allan.boite@gmail.com>
2020-08-02 23:11:47 +0200

    ajustement

--- projet3/src/projet3/Main.java
+++ projet3/src/projet3/Main.java
@@ -5,8 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		for(Day moment : Day.values()) {
-			System.out.println(" Nous sommes actuellement le "
-					+ " " + moment.name().toLowerCase() + " il est actuellement : " + moment.getHeure() + "h . " + moment.getMessage());
+			System.out.println(" Nous sommes actuellement le " + moment.name().toLowerCase() + " il est actuellement : " + moment.getHeure() + "h . " + moment.getMessage());
 		}
 		
 		
