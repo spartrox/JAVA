03075a417eef20eeb49385cdae69b6bc8525692d
Allan Boite <allan.boite@gmail.com>
2020-08-05 19:54:05 +0200

    Array liste suite

--- projet3/src/projet3/Main.java
+++ projet3/src/projet3/Main.java
@@ -6,11 +6,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList list = new ArrayList();
-		list.add(25);
+		ArrayList<String> list = new ArrayList<>();    // <> mettre ce que l'on veux stocker dans le tableau : String, Integer...
+		list.add(25 " 25 ne fonctionnera pas");
 		list.add("Text");
-		list.add(new Object());
+		list.add("Le texte fonctionne");
 		
+		System.out.println(list);
+		
+		list.remove(0);   // PERMET DE SUPPRIMER UN ELEMENT DANS LE TABLEAU
+		
+		list.clear();     // PERMET DE SUPPRIMER TOUTES LES DONNÉES DU TABLEAU
+		
+		 
 		System.out.println(list);
 				
 		
