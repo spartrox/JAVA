f5f72add0414b2e9425b62370779ade1792c394b
Allan Boite <allan.boite@gmail.com>
2020-08-05 22:14:05 +0200

    Array liste suite

--- projet3/src/projet3/Main.java
+++ projet3/src/projet3/Main.java
@@ -1,25 +1,21 @@
 package projet3;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		ArrayList<String> list = new ArrayList<>();    // <> mettre ce que l'on veux stocker dans le tableau : String, Integer...
-		list.add(25 " 25 ne fonctionnera pas");
-		list.add("Text");
-		list.add("Le texte fonctionne");
-		
-		System.out.println(list);
-		
-		list.remove(0);   // PERMET DE SUPPRIMER UN ELEMENT DANS LE TABLEAU
-		
-		list.clear();     // PERMET DE SUPPRIMER TOUTES LES DONNÉES DU TABLEAU
-		
-		 
-		System.out.println(list);
 				
+		
+		
+		Map<String, Integer> ages = new HashMap<>();
+		
+		ages.put("Spartrox", 22);
+		ages.put("Sabrina", 23);
+		
+		System.out.println(ages);
 		
 		
 		
